532b2b39a24e907e6c0ba083666131cf92fb9739
xjqsh <59763518+xjqsh@users.noreply.github.com>
2024-11-10 21:55:50 +0800

    临时的reload实现，只完成了客户端部分

--- src/main/java/com/tacz/guns/command/sub/ReloadCommand.java
+++ src/main/java/com/tacz/guns/command/sub/ReloadCommand.java
@@ -3,10 +3,16 @@ package com.tacz.guns.command.sub;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.tacz.guns.client.resource.ClientAssetsManager;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
+import org.apache.commons.lang3.time.StopWatch;
+
+import java.util.concurrent.TimeUnit;
 
 public class ReloadCommand {
     private static final String RELOAD_NAME = "reload";
@@ -18,15 +24,15 @@ public class ReloadCommand {
     }
 
     private static int reloadAllPack(CommandContext<CommandSourceStack> context) {
-//        StopWatch watch = StopWatch.createStarted();
-//        {
-//            DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> ClientReloadManager::reloadAllPack);
+        StopWatch watch = StopWatch.createStarted();
+        {
+            DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ClientAssetsManager::reloadAllPack);
 //            DistExecutor.unsafeRunWhenOn(Dist.DEDICATED_SERVER, () -> () -> DedicatedServerReloadManager.reloadFromCommand(context));
-//        }
-//        watch.stop();
-//        double time = watch.getTime(TimeUnit.MICROSECONDS) / 1000.0;
+        }
+        watch.stop();
+        double time = watch.getTime(TimeUnit.MICROSECONDS) / 1000.0;
 //        if (context.getSource().getEntity() instanceof ServerPlayer serverPlayer) {
-//            serverPlayer.sendSystemMessage(Component.translatable("commands.tacz.reload.success", time));
+//
 //            if (OtherConfig.DEFAULT_PACK_DEBUG.get()) {
 //                serverPlayer.sendSystemMessage(Component.translatable("commands.tacz.reload.overwrite_off"));
 //                serverPlayer.sendSystemMessage(Component.translatable("commands.tacz.reload.overwrite_command.off"));
@@ -38,7 +44,7 @@ public class ReloadCommand {
 //        }
 //        GunMod.LOGGER.info("Model loading time: {} ms", time);
         if (context.getSource().getEntity() instanceof ServerPlayer serverPlayer) {
-            serverPlayer.sendSystemMessage(Component.literal("WIP"));
+            serverPlayer.sendSystemMessage(Component.translatable("commands.tacz.reload.success", time));
         }
         return Command.SINGLE_SUCCESS;
     }
